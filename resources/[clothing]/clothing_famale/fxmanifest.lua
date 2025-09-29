3b5c9ec1a1766e6780adc3ce4bad20332ca64ce8
iHan270796 <evidariswan@gmail.com>
2025-09-29 16:11:51 +0700

    update samudra roleplay

---
+++ resources/[clothing]/clothing_famale/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+files {
+    'mp_f_freemode_01_b_clothes.meta',
+    'mp_f_freemode_01_mp_f_rstore.meta',
+    'mp_f_freemode_01_mp_f_lonewolf.meta',
+    'mp_f_freemode_01_mp_f_policepack.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_lonewolf.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_b_clothes.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_rstore.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_policepack.meta'
