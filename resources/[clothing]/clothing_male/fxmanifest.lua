811276df2cb5be2abd26517a789012c0f290af34
iHan270796 <evidariswan@gmail.com>
2025-09-29 16:16:31 +0700

    update aseet samudra roleplay

---
+++ resources/[clothing]/clothing_male/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+files {
+    'mp_m_freemode_01_mp_m_rstore.meta',
+    'mp_m_freemode_01_mp_m_aspex_mechanicuniform.meta',
+    'mp_m_freemode_01_mp_m_policepack.meta',
+    'mp_m_freemode_01_mp_m_lonewolf.meta',
+    'mp_m_freemode_01_mp_m_takashima.meta',
+    'pedalternatevariations.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_lonewolf.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_rstore.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_aspex_mechanicuniform.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_policepack.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_takashima.meta'
+data_file 'ALTERNATE_VARIATIONS_FILE' 'pedalternatevariations.meta'
