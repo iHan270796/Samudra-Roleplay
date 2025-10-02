651fe03f57701130b7d1c2e3f78098935b060a0d
iHan270796 <evidariswan@gmail.com>
2025-10-02 09:37:45 +0700

    update kendaraan

---
+++ resources/[kendaraan]/samudra_vehiclesounds/fxmanifest.lua
@@ -0,0 +1,111 @@
+fx_version "adamant"
+game "gta5"
+
+-- jangan di otak atik yang ini
+files {
+	'audioconfig/*.dat151',
+    'audioconfig/*.dat151.nametable',
+    'audioconfig/*.dat151.rel',
+    'audioconfig/*.dat10.nametable',
+    'audioconfig/*.dat10.rel',
+    'audioconfig/*.dat10',
+    'audioconfig/*.dat54.nametable',
+    'audioconfig/*.dat54.rel',
+    'audioconfig/*.dat54',
+    'audioconfig/*.dat',
+    'sfx/**/*.awc',
+}
+--JGMECHANIC
+data_file 'AUDIO_GAMEDATA' 	'audioconfig/488sound_game.dat'
+data_file 'AUDIO_GAMEDATA' 	'audioconfig/apollosv8_game.dat'
+data_file 'AUDIO_GAMEDATA' 	'audioconfig/avesvv12_game.dat'
+data_file 'AUDIO_GAMEDATA' 	'audioconfig/diablov12_game.dat'
+data_file 'AUDIO_GAMEDATA' 	'audioconfig/f40v8_game.dat'
+data_file 'AUDIO_GAMEDATA' 	'audioconfig/f50v12_game.dat' 
+data_file 'AUDIO_GAMEDATA' 	'audioconfig/ferrarif12_game.dat'
+data_file 'AUDIO_GAMEDATA' 	'audioconfig/gt3flat6_game.dat'
+data_file 'AUDIO_GAMEDATA' 	'audioconfig/k20a_game.dat'
+data_file 'AUDIO_GAMEDATA' 	'audioconfig/lambov10_game.dat'
+data_file 'AUDIO_GAMEDATA' 	'audioconfig/m158huayra_game.dat'
+data_file 'AUDIO_GAMEDATA' 	'audioconfig/m297zonda_game.dat'
+data_file 'AUDIO_GAMEDATA' 	'audioconfig/mclarenv8_game.dat'
+data_file 'AUDIO_GAMEDATA' 	'audioconfig/murciev12_game.dat'
+data_file 'AUDIO_GAMEDATA' 	'audioconfig/musv8_game.dat'
+data_file 'AUDIO_GAMEDATA' 	'audioconfig/perfov10_game.dat'
+data_file 'AUDIO_GAMEDATA' 	'audioconfig/predatorv8_game.dat'
+data_file 'AUDIO_GAMEDATA' 	'audioconfig/rotary7_game.dat'
+data_file 'AUDIO_GAMEDATA' 	'audioconfig/toysupmk4_game.dat'
+data_file 'AUDIO_GAMEDATA' 	'audioconfig/urusv8_game.dat'
+data_file 'AUDIO_GAMEDATA' 	'audioconfig/viperv10_game.dat'
+data_file 'AUDIO_GAMEDATA'  'audioconfig/r35sound_game.dat'
+data_file 'AUDIO_GAMEDATA'  'audioconfig/488sound_game.dat'
+data_file 'AUDIO_GAMEDATA'  'audioconfig/gtaspanov10_game.dat'
+data_file 'AUDIO_GAMEDATA'  'audioconfig/shonen_game.dat'
+data_file 'AUDIO_GAMEDATA'  'audioconfig/veyronsound_game.dat'
+data_file 'AUDIO_GAMEDATA'  'audioconfig/sestov10_game.dat'
+
+--------------------------------------------------------------- Audio Sound Data
+
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/488sound_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/apollosv8_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/avesvv12_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/diablov12_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/f40v8_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/f50v12_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/ferrarif12_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/gt3flat6_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/k20a_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/lambov10_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/m158huayra_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/m297zonda_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/mclarenv8_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/murciev12_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/musv8_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/perfov10_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/predatorv8_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/rotary7_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/toysupmk4_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/urusv8_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/viperv10_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/r35sound_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/488sound_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/sestov10_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/gtaspanov10_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/shonen_sounds.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/veyronsound_sounds.dat'
+
+--------------------------------------------------------------- Audio Synthersizer Data
+
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/gt3flat6_amp.dat'
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/k20a_amp.dat'
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/m158huayra_amp.dat'
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/m297zonda_amp.dat'
+
+---------------------------------------------------------------  Audio Wavepacks
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_488sound'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_apollosv8'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_avesvv12'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_diablov12'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_f40v8'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_f50v12'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ferrarif12'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_gt3flat6'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_k20a'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_lambov10'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_m158huayra'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_m297zonda'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_mclarenv8'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_murciev12'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_musv8'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_perfov10'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_predatorv8'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_rotary7'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_toysupmk4'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_urusv8'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_viperv10'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_r35sound'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_488sound'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_veyronsound'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_shonen'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_sestov10'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_gtaspanov10'
---
+++ resources/[kendaraan]/samudra_vehiclesounds_dua/fxmanifest.lua
@@ -0,0 +1,127 @@
+fx_version "adamant"
+game "gta5"
+
+-- jangan di otak atik yang ini
+files {
+	'audioconfig/*.dat151',
+    'audioconfig/*.dat151.nametable',
+    'audioconfig/*.dat151.rel',
+    'audioconfig/*.dat10.nametable',
+    'audioconfig/*.dat10.rel',
+    'audioconfig/*.dat10',
+    'audioconfig/*.dat54.nametable',
+    'audioconfig/*.dat54.rel',
+    'audioconfig/*.dat54',
+    'audioconfig/*.dat',
+    'sfx/**/*.awc',
+}
+
+-- khusus game
+data_file "AUDIO_GAMEDATA" "audioconfig/g55_game.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/nsr2teng_game.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/rb26dett_game.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/gallardov10_game.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/kc61evo10rally_game.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/ta103ninjah2r_game.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/bnr34ffeng_game.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/tagt3flat6_game.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/aq67merm177_game.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/tamustanggt50_game.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/str6sr20_game.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/lg57mustangtv8_game.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/lamavgineng_game.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/a80ffeng_game.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/brisket_game.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/aq65subej257_game.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/ta488f154_game.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/r35sound_game.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/szkgsxryoshimuraeng_game.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/bnkdmcrb26eng_game.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/f136_game.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/rx7bpeng_game.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/aq10nisvr38dett_game.dat"
+
+-- khusus sound
+data_file "AUDIO_SOUNDDATA" "audioconfig/rb26dett_sounds.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/g55_sounds.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/nsr2teng_sounds.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/gallardov10_sounds.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/kc61evo10rally_sounds.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/ta103ninjah2r_sounds.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/bnr34ffeng_sounds.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/tagt3flat6_sounds.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/aq67merm177_sounds.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/tamustanggt50_sounds.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/str6sr20_sounds.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/lg57mustangtv8_sounds.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/lamavgineng_sounds.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/a80ffeng_sounds.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/brisket_sounds.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/aq65subej257_sounds.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/ta488f154_sounds.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/r35sound_sounds.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/szkgsxryoshimuraeng_sounds.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/bnkdmcrb26eng_sounds.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/f136_sounds.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/rx7bpeng_sounds.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/aq10nisvr38dett_sounds.dat"
+
+-- khusus amp
+data_file "AUDIO_SYNTHDATA" "audioconfig/rb26dett_amp.dat"
+data_file "AUDIO_SYNTHDATA" "audioconfig/nsr2teng_amp.dat"
+data_file "AUDIO_SYNTHDATA" "audioconfig/kc61evo10rally_amp.dat"
+data_file "AUDIO_SYNTHDATA" "audioconfig/bnr34ffeng_amp.dat"
+data_file "AUDIO_SYNTHDATA" "audioconfig/lamavgineng_amp.dat"
+data_file "AUDIO_SYNTHDATA" "audioconfig/a80ffeng_amp.dat"
+data_file "AUDIO_SYNTHDATA" "audioconfig/szkgsxryoshimuraeng_amp.dat"
+data_file "AUDIO_SYNTHDATA" "audioconfig/bnkdmcrb26eng_amp.dat"
+data_file "AUDIO_SYNTHDATA" "audioconfig/f136_amp.dat"
+data_file "AUDIO_SYNTHDATA" "audioconfig/rx7bpeng_amp.dat"
+
+
+-- khusus sfx
+data_file "AUDIO_WAVEPACK" "sfx/dlc_f136"
+
+data_file "AUDIO_WAVEPACK" "sfx/dlc_aq10nisvr38dett"
+
+data_file "AUDIO_WAVEPACK" "sfx/dlc_rx7bpeng"
+
+data_file "AUDIO_WAVEPACK" "sfx/dlc_bnkdmcrb26eng"
+
+data_file "AUDIO_WAVEPACK" "sfx/dlc_r35sound"
+
+data_file "AUDIO_WAVEPACK" "sfx/dlc_szkgsxryoshimuraeng"
+
+data_file "AUDIO_WAVEPACK" "sfx/dlc_g55"
+
+data_file "AUDIO_WAVEPACK" "sfx/dlc_nsr2teng"
+
+data_file "AUDIO_WAVEPACK" "sfx/dlc_rb26dett"
+
+data_file "AUDIO_WAVEPACK" "sfx/dlc_gallardov10"
+
+data_file "AUDIO_WAVEPACK" "sfx/dlc_kc61evo10rally"
+
+data_file "AUDIO_WAVEPACK" "sfx/dlc_ta103ninjah2r"
+
+data_file "AUDIO_WAVEPACK" "sfx/dlc_bnr34ffeng"
+
+data_file "AUDIO_WAVEPACK" "sfx/dlc_tagt3flat6"
+
+data_file "AUDIO_WAVEPACK" "sfx/dlc_aq67merm177"
+
+data_file "AUDIO_WAVEPACK" "sfx/dlc_tamustanggt50"
+
+data_file "AUDIO_WAVEPACK" "sfx/dlc_str6sr20"
+
+data_file "AUDIO_WAVEPACK" "sfx/dlc_lg57mustangtv8"
+
+data_file "AUDIO_WAVEPACK" "sfx/dlc_lamavgineng"
+
+data_file "AUDIO_WAVEPACK" "sfx/dlc_a80ffeng"
+
+data_file "AUDIO_WAVEPACK" "sfx/dlc_brisket"
+
+data_file "AUDIO_WAVEPACK" "sfx/dlc_aq65subej257"
+
+data_file "AUDIO_WAVEPACK" "sfx/dlc_ta488f154"
